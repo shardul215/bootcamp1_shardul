78a8bd66b6abf82deb669ba13ff2b405a18c90bf
shardul215 <98656722+shardul215@users.noreply.github.com>
2025-03-18 17:16:29 +0530

    added grading program file

---
+++ grading program.py
@@ -0,0 +1,11 @@
+a= int(input("Enter your marks "))
+if(a>=90):
+    print("you got ",a," marks and your grade is A")
+elif(a>=80):
+    print("you got ",a," marks and your grade is B")
+elif(a>=70):
+    print("you got ",a," marks and your grade is C")
+elif(a>=60):
+    print("you got ",a," marks and your grade is D")
+else:
+    print("Your marks are",a,"\n!!Please give the test again!!")
